bb1440371fece31f3479dc98e0e44adaa6a4e8b3
quocbaodoan <quocbaodoan1999@gmail.com>
2021-01-08 18:49:32 +0700

    update

--- src/components/CartContent.js
+++ src/components/CartContent.js
@@ -11,32 +11,32 @@ export default function CartContent() {
     return (
         <div className='cart'>
             <div className='cart__inner'>
-                <img src={waterbottle}/>
+                <img src={waterbottle} />
                 <h2>Hydros</h2>
                 <div className="qty">
                     <div>
-                        <input className="btn-triangle btn-decrement mx-1" onClick={() => setQty(qty - 1)} disabled={(qty <= 1) ? true : false}/>
+                        <input className="btn-triangle btn-decrement mx-1" onClick={() => setQty(qty - 1)} disabled={(qty <= 1) ? true : false} />
                     </div>
                     <div>
-                        <span className="btn-number-cart mx-1" style={{color: "#335eea"}}>{qty}</span> 
+                        <span className="btn-number-cart mx-1" style={{ color: "#335eea" }}>{qty}</span>
                     </div>
                     <div>
-                        <input className="btn-triangle btn-increment mx-1" onClick={() => setQty(qty + 1)}/>
+                        <input className="btn-triangle btn-increment mx-1" onClick={() => setQty(qty + 1)} />
                     </div>
                 </div>
-                <h3 className="mt-3"  style={{marginBottom: "1rem", color: "#335eea", fontSize: '1.2rem', fontWeight: '400'}}>
-                    {convertToString(990000 * qty )}đ
+                <h3 className="mt-3" style={{ marginBottom: "1rem", color: "#335eea", fontSize: '1.2rem', fontWeight: '400' }}>
+                    {convertToString(700000 * qty)}đ
                 </h3>
-                <PaypalButton total={990000 * qty}/>
+                <PaypalButton total={Math.round((700000 * qty) / 23000)} />
             </div>
         </div>
     )
 }
 
-function convertToString(value){
+function convertToString(value) {
     let resString = value.toString();
     let resFinal = "";
-    while(resString.length > 3){
+    while (resString.length > 3) {
         resFinal = "." + resString.substr(resString.length - 3, resString.length - 1) + resFinal;
         resString = resString.substr(0, resString.length - 3);
     }
